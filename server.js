ef8c67742be1c6540527ab6f86f272b779bee699
Bhoomika <bhoomika1158.be23@chitkarauniversity.edu.in>
2026-02-10 12:23:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -72,5 +72,9 @@ app.get('/health',(req,res)=>{
     });
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server running on port", PORT);
+});
 
-app.listen(3000,()=>console.log("Running on 3000"));
+
